24a5892903b9944309eb054a4f951478eb9b6900
sathwikhj <sathwikhj2011@gmail.com>
2024-04-08 12:14:54 +0530

    Add logging features

--- Using Installed Appflow/main.py
+++ Using Installed Appflow/main.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import logging
 from datetime import datetime
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -7,6 +8,9 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.errors import HttpError
+
+# Configure logging
+logging.basicConfig(filename='backup.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 creds = None
@@ -24,17 +28,17 @@ if not creds or not creds.valid:
         token.write(creds.to_json())
 
 try:
-    service=build("drive","v3",credentials=creds)
+    service = build("drive", "v3", credentials=creds)
     response = service.files().list(
-        q="name='<Folder-Name>' and mimeType='application/vnd.google-apps.folder'", #In the name tag, provide the name for backup folder 
+        q="name='<Folder-Name>' and mimeType='application/vnd.google-apps.folder'",  # In the name tag, provide the name for backup folder
         spaces="drive"
     ).execute()
-    
-    print("Response:", response) 
-    
+
+    logging.info("Response: %s", response)
+
     if 'files' in response and not response['files']:
         file_metadata = {
-            "name": "<Folder-Name>", #Name of Google Drive folder where you want the backups to be stored
+            "name": "<Folder-Name>",  # Name of Google Drive folder where you want the backups to be stored
             "mimeType": "application/vnd.google-apps.folder"
         }
         folder = service.files().create(body=file_metadata, fields="id").execute()
@@ -43,7 +47,7 @@ try:
         folder_id = response['files'][0]['id']
     else:
         raise Exception("Response does not contain 'files' key or is empty.")
-    
+
     backup_date_folder_name = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
     file_metadata = {
         "name": backup_date_folder_name,
@@ -53,25 +57,25 @@ try:
     datetime_folder = service.files().create(body=file_metadata, fields="id").execute()
     datetime_folder_id = datetime_folder.get('id')
 
-    assignment_folder_path = '<Folder-path>' #Provide path to the folder on the local system which you want backed up
+    assignment_folder_path = '<Folder-path>'  # Provide path to the folder on the local system which you want backed up
     if not os.path.exists(assignment_folder_path):
-        print("File not found")
+        logging.error("File not found")
     else:
-        count=0
-        for filename in os.listdir('<Name of the directory>'): #Provide the name of the directory 
+        count = 0
+        for filename in os.listdir('<Name of the directory>'):  # Provide the name of the directory
             file_metadata = {
                 "name": filename,
                 "parents": [datetime_folder_id]
             }
-            media = MediaFileUpload(f"<Name of the directory>/{filename}") #Provide the name of the directory 
+            media = MediaFileUpload(f"<Name of the directory>/{filename}")  # Provide the name of the directory
             upload_file = service.files().create(
                 body=file_metadata,
                 media_body=media,
                 fields='id'
             ).execute()
-            print("Backed Up file:", filename)
-            count+=1
-        print("Files in the folder: "+str(count))
+            logging.info("Backed Up file: %s", filename)
+            count += 1
+        logging.info("Files in the folder: %d", count)
 
 except Exception as e:
-    print("Error:", str(e))
+    logging.error("Error: %s", str(e))
--- Using Service Account/main.py
+++ Using Service Account/main.py
@@ -1,47 +1,50 @@
 import os
 import os.path
+import logging
 from datetime import datetime
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
+# Configure logging
+logging.basicConfig(filename='backup.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 creds = None
-
 
 creds = Credentials.from_service_account_file("credentials.json", scopes=SCOPES)
 
 service = build("drive", "v3", credentials=creds)
 
-try: 
+try:
     backup_date_folder_name = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
     file_metadata = {
         "name": backup_date_folder_name,
-        "parents": ["<folder-id>"], #Add folder id as mentioned in the image
+        "parents": ["<folder-id>"],  # Add folder id as mentioned in the image
         "mimeType": "application/vnd.google-apps.folder"
     }
     datetime_folder = service.files().create(body=file_metadata, fields="id").execute()
     datetime_folder_id = datetime_folder.get('id')
 
-    assignment_folder_path = '<Folder-path>' #Provide path to the folder on the local system which you want backed up
+    assignment_folder_path = '<Folder-path>'  # Provide path to the folder on the local system which you want backed up
     if not os.path.exists(assignment_folder_path):
-        print("File not found")
+        logging.error("File not found")
     else:
-        count=0
-        for filename in os.listdir('<Name of the directory>'): #Provide the name of the directory 
+        count = 0
+        for filename in os.listdir('<Name of the directory>'):  # Provide the name of the directory
             file_metadata = {
                 "name": filename,
                 "parents": [datetime_folder_id]
             }
-            media = MediaFileUpload(f"<Name of the directory>/{filename}") #Provide the name of the directory 
+            media = MediaFileUpload(f"<Name of the directory>/{filename}")  # Provide the name of the directory
             upload_file = service.files().create(
                 body=file_metadata,
                 media_body=media,
                 fields='id'
             ).execute()
-            print("Backed Up file:", filename)
-            count+=1
-        print("Files in the folder: "+str(count))
+            logging.info("Backed Up file: %s", filename)
+            count += 1
+        logging.info("Files in the folder: %d", count)
 
 except Exception as e:
-    print("Error:", str(e))
+    logging.error("Error: %s", str(e))
